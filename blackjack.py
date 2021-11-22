4c6a527e0c8f6dc75875f789c07ba13e26b3df6f
maariagarrcia <mrrmn@myuax.com>
2021-11-22 18:31:11 +0100

    empezar 1 funcion

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,33 @@
+#
+# P A G I N A S   C O N S U L T A D A S
+#
+# Tipo de Datos: List ............... https://www.w3schools.com/python/python_lists.asp
+# Tipo de Datos: Dictionary ......... https://pythonexamples.org/python-dictionary-operations/
+#
+# Método de Random "choice()" ....... https://www.w3schools.com/python/ref_random_choice.asp
+# Método de Random "sample" ......... https://www.w3schools.com/python/ref_random_sample.asp
+# Método de String "join()".......... https://www.w3schools.com/python/ref_string_join.asp
+#
+# Funcion "format()"................. https://www.w3schools.com/python/ref_func_format.asp
+# Funcioon "sorted()"................ https://www.w3schools.com/python/ref_func_sorted.asp
+# Funcion "sum()" ................... https://www.w3schools.com/python/ref_func_sum.asp
+#
+
+# D E S C R I P C I O N     T A R E A    R E A L I Z A D A:
+#
+# 0) Estudiar el código suministrado en la tarea 
+# 1) Buscar documentación sobre los tipos de datos, funcions y métodos que se van a necesitar
+# 2) Crear un nuevo código usando lo aprendido previamente:
+#       · Hacer un código lo más legible posible
+#       · Hacer el código lo más modular posible usando funciones (no se requieren módulos)
+# 3) Documentar la tarea realizada sobre el propio código para facilitar el estudio posterior
+#
+#
+
 from random import choice, sample
+
+
+# En este diccionario se guardan las cartas y su puntuacion
 
 cartas = {
     chr(0x1f0a1): 11,
@@ -16,6 +45,15 @@ cartas = {
     chr(0x1f0ae): 10,
 }
 
-print("Cartas: {}".format(" ".join(cartas.keys())))
-print("Puntos: {}".format(list(cartas.values())))
+#  F U N C I O N E S
+
+def mostrarCartas():
+    print(Fore.YELLOW)
+    print (" Relacion de cartas y puntuaciones")
+    print (" Ordenadas por puntuacion descendente")
+    for key, value in sorted(cartas.items(), cartas.value(), reverse= True):
+        print (value, key)
+    
+    print("Cartas: {}".format(" ".join(cartas.keys())))
+    print("Puntos: {}".format(list(cartas.values())))
 
